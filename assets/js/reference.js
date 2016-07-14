ef4993a30288e7e32f6a460cade6aa769c3cd9a6
Sandeep Kumar <sandeep.kumar@cygrp.com>
2016-07-15 00:55:12 +0530

    alert popup modified

--- assets/js/reference.js
+++ assets/js/reference.js
@@ -143,28 +143,8 @@ document.getElementById('export-usfm').addEventListener('click', function (e) {
 
 // Alert Model Function for dynamic message
 function alertModal(heading, formContent) {
-  html =  '<div id="dynamicModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="confirm-modal" aria-hidden="true">';
-  html += '<div class="modal-dialog">';
-  html += '<div class="modal-content">';
-  html += '<div class="modal-header">';
-  html += '<a class="close" data-dismiss="modal">×</a>';
-  html += '<h4>'+heading+'</h4>'
-  html += '</div>';
-  html += '<div class="modal-body">';
-  html += '<p>';
-  html += formContent;
-  html += '</div>';
-  html += '<div class="modal-footer">';
-  html += '<span class="btn btn-primary" data-dismiss="modal">Close</span>';
-  html += '</div>';  // content
-  html += '</div>';  // dialog
-  html += '</div>';  // footer
-  html += '</div>';  // modalWindow
-  $('body').append(html);
+  $("#heading").html(heading);
+  $("#content").html(formContent);
   $("#dynamicModal").modal();
-  $("#dynamicModal").modal('show');
-
-  $('#dynamicModal').on('hidden.bs.modal', function (e) {
-      $(this).remove();
-  });
+  $("#dynamicModal").toggle();
 }
